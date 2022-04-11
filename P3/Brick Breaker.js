a64dc4d69027abf9cfbac7c003a0f7f226715aa5
Alejandro Grande <a.grandeg.2019@alumnos.urjc.es>
2022-04-11 02:07:52 +0200

    Básico

--- P3/Brick Breaker.js
+++ P3/Brick Breaker.js
@@ -10,7 +10,15 @@ const ctx = canvas.getContext("2d");
 let a = 0;
 let b = 0;
 let x = 346;
-let y = 850;
+let y = 830;
+let v = 296;
+let w = 850;
+let score = 0;
+let vidas = 3;
+let pause = "";
+
+var img = new Image();
+img.src = "Heart.png";
 
 //-- Velocidades del objeto
 let velx = 0;
@@ -55,31 +63,56 @@ const ladrillos = [];
 
 
 window.onkeydown = (e) => {
-    if (x == 346 && y == 850 && e.key == ' ') {
+    if (x == 346 && y == 830 && e.key == ' ' && vidas != 0) {
         velx = 4;
         vely = -4;
+    } else if (vidas == 0 && e.key == ' ') {
+        velx = 4;
+        vely = -4;
+        score = 0;
+        vidas = 3;
+        text = "";
+        pause = "";
+        for (let i = 0; i < LADRILLO.F; i++) {
+            for (let j = 0; j < LADRILLO.C; j++) {
+        ladrillos[i][j].visible = true;
+            }
+        }
     } else if (e.key == 'r'){
         velx = 0;
         vely = 0;
         x = 346;
-        y = 850;
+        y = 830;
+        v = 296;
+        w = 850;
+        score = 0;
+        vidas = 3;
+        text = "";
+        pause = "";
+        for (let i = 0; i < LADRILLO.F; i++) {
+            for (let j = 0; j < LADRILLO.C; j++) {
         ladrillos[i][j].visible = true;
-        console.log("Restart");
+            }
+        }
     } else if (velx != 0 && vely != 0 && e.key == 'Escape') {
         a = velx;
         b = vely;
         velx = 0;
         vely = 0;
-        console.log("Pause");
+        pause = "PAUSE";
     } else if (velx == 0 && vely == 0 && x != 346 && y != 850 && e.key == 'Escape') {
         velx = a;
         vely = b;
+        pause = "";
         console.log("Resume");
     } else if (x == 346 && y == 850 && e.key == 'Escape') {
         velx = 0;
         vely = 0;
+    } else if (e.key == 'ArrowLeft' && velx != 0 && vely != 0 && v >= 0) {
+        v = v - 5;
+    } else if (e.key == 'ArrowRight' && velx != 0 && vely != 0 && v <= canvas.width - 120) {
+        v = v + 5;
     }
-    console.log(e.key);
 }
 function update() {
     
@@ -98,18 +131,68 @@ function update() {
     
     //-- 2) Borrar el canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+                ctx.font= "50pt Comic Sans";
+                ctx.fillText("Puntuación: ", 20, 60, 200);
+                ctx.fillText(score, 220, 60, 100);
+
+    ctx.fillStyle = "white";
+        ctx.font = "100pt Comic Sans";
+        ctx.fillText(pause, 165, 450, 400);
+
+    if (y == (w - 20) && (v <= x + 20) && (x <= (v + 120))) {
+        vely = -vely;
+    }
+    
+    if (y + 25 >= canvas.height) {
+        velx = 0;
+        vely = 0;
+        x = 346;
+        y = 830;
+        v = 296;
+        w = 850;
+        vidas = vidas - 1;
+    }
+
+    if (vidas == 3) {
+        ctx.drawImage(img, 613, 0);
+        ctx.drawImage(img, 523, 0);
+        ctx.drawImage(img, 433, 0);
+    } else if (vidas == 2) {
+        ctx.drawImage(img, 523, 0);
+        ctx.drawImage(img, 433, 0);
+    } else if (vidas == 1) {
+        ctx.drawImage(img, 433, 0);
+    } else if (vidas == 0) {
+        text = "GAME OVER";
+            ctx.fillStyle = "white";
+            ctx.font = "100pt Comic Sans";
+            ctx.fillText(text, 165, 450, 400);
+    }
 
     //-- Dibujar ladrillos
     for (let i = 0; i < LADRILLO.F; i++) {
         for (let j = 0; j < LADRILLO.C; j++) {
 
-            if (x <= (ladrillos[i][j].x + 73) && x > (ladrillos[i][j].x) && y <= (ladrillos[i][j].y + 35) && ladrillos[i][j].visible == true){
+            if (x <= (ladrillos[i][j].x + 63) && x > (ladrillos[i][j].x - 10) && y <= (ladrillos[i][j].y + 38) && y > (ladrillos[i][j].y + 35) && ladrillos[i][j].visible == true){
                 vely = -vely;
                 ladrillos[i][j].visible = false;
+                score = score + 1;
+                
+            } else if (x <= (ladrillos[i][j].x + 63) && x > (ladrillos[i][j].x - 10) && y <= (ladrillos[i][j].y) && y > (ladrillos[i][j].y - 25) && ladrillos[i][j].visible == true){
+                vely = -vely;
+                ladrillos[i][j].visible = false;
+                score = score + 1;
             
-            } else if (y <= (ladrillos[i][j].y + 35) && y > (ladrillos[i][j].y) && x <= (ladrillos[i][j].x) && ladrillos[i][j].visible == true){
+            } else if (y <= (ladrillos[i][j].y + 35) && y > (ladrillos[i][j].y - 10) && x <= (ladrillos[i][j].x + 68) && x > (ladrillos[i][j].x + 63) && ladrillos[i][j].visible == true){
                 velx = -velx;
                 ladrillos[i][j].visible = false;
+                score = score + 1;
+
+            } else if (y <= (ladrillos[i][j].y + 35) && y > (ladrillos[i][j].y - 10) && x <= (ladrillos[i][j].x) && x > (ladrillos[i][j].x - 10) && ladrillos[i][j].visible == true){
+                velx = -velx;
+                ladrillos[i][j].visible = false;
+                score = score + 1;
             }
         }
     }
@@ -120,6 +203,7 @@ function update() {
 
             ctx.beginPath();
             ctx.rect(x, y, 20, 20); // Bola
+            ctx.rect(v, w, 120, 20); // Pala
             ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
             ctx.fillStyle = 'white';
             ctx.fill();
